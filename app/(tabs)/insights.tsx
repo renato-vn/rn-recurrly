c87576e9f8ef42d5f12e2ff88cba74df47d326a2
renato-vn <renatovicencion@gmail.com>
2026-09-02 19:40:33 -0400

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,14 @@
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Insights = () => {
+  return (
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text>Insights</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Insights;
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,14 @@
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Subscriptions = () => {
+  return (
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text>Subscriptions</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Subscriptions;
